3d4be978375423bfe5b481f249c6c83b5d6059e7
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-07-13 01:53:11 +0300

    additional stuff

--- Task4.js
+++ Task4.js
@@ -1,8 +1,13 @@
 function capitalize(string){
+    if (typeof(string) !== 'string' || string === ''){
+        return 'please enter a word'
+    } else {
     return string.toLowerCase().split(' ').map( (word)=>{
         return (word.charAt(0).toUpperCase() + word.slice(1));
-    }).join(' ')
+    }).join(' ')};
 };
 
 
 module.exports = capitalize;
+
+console.log(capitalize(''))
--- Task4.test.js
+++ Task4.test.js
@@ -3,4 +3,9 @@ const Capital = require('./Task4');
 
 test('make the first letter captal', ()=> {
     expect(Capital('name')).toMatch('Name');
+    expect(Capital(1)).toBe('please enter a word');
+    expect(Capital('')).toBe('please enter a word');
+    
 });
+
+
